49e4a0d5afb576989cd1f18f83d630ae3421195d
Gio <sethg@ipi.org>
2021-11-06 22:21:05 -0500

    Basic hygiene

--- remove_duplicate_lines.py
+++ remove_duplicate_lines.py
@@ -2,37 +2,39 @@ import sublime
 import sublime_plugin
 from collections import OrderedDict
 
+
 class RemoveDuplicateLinesCommand(sublime_plugin.TextCommand):
-  def dedupe(self, selection, edit):
-    """
-    Removes duplicate lines from the selected region by splitting it into lines,
-    adding them into an `OrderedDict` which automatically removes duplicates,
-    and combines everything back into a string with newlines
-    """
+    def dedupe(self, selection, edit):
+        """
+        Removes duplicate lines from the selected region by splitting it into lines,
+        adding them into an `OrderedDict` which automatically removes duplicates,
+        and combines everything back into a string with newlines
+        """
 
-    selection = self.view.expand_by_class(selection, sublime.CLASS_LINE_END)
+        selection = self.view.expand_by_class(selection, sublime.CLASS_LINE_END)
+  
+        lines = self.view.substr(selection).splitlines()
+  
+        text = '\n'.join(OrderedDict.fromkeys(lines))
+  
+        self.view.replace(edit, selection, text)
+  
+    def run(self, edit):
+        """
+        Removes duplicate lines so that each line contains a unique string
+        If one or more multiline regions are selected, it removes the duplicate
+        lines that are confined within each of the regions
+        """
 
-    lines = self.view.substr(selection).splitlines()
+        non_empty_selections = [
+            selection for selection in self.view.sel()
+            if not selection.empty()
+        ]
 
-    text = '\n'.join(OrderedDict.fromkeys(lines))
+        if non_empty_selections:
+            for selection in reversed(non_empty_selections):
+                self.dedupe(selection, edit)
 
-    self.view.replace(edit, selection, text)
-
-  def run(self, edit):
-    """
-    Removes duplicate lines so that each line contains a unique string
-    If one or more multiline regions are selected, it removes the duplicate
-    lines that are confined within each of the regions
-    """
-
-    non_empty_selections = [selection for selection in self.view.sel()
-                            if not selection.empty()]
-
-    if non_empty_selections:
-      [self.dedupe(selection, edit)
-       for selection in reversed(non_empty_selections)]
-
-    else:
-      entire_file = sublime.Region(0, self.view.size())
-
-      self.dedupe(entire_file, edit)
+        else:
+            entire_file = sublime.Region(0, self.view.size())
+            self.dedupe(entire_file, edit)
